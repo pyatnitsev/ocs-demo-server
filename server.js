1591030b489ac90e61d7e2f6f7fac10d49993268
Danil Pyatnitsev <danil@pyatnitsev.ru>
2025-05-30 19:53:36 +0300

    update ws server connected to OSC; added more logs added more docs

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const WebSocket = require('ws');
 
 const WS_PORT = process.env.WS_PORT || 8080;
 const OSC_HOST = process.env.OSC_HOST || '127.0.0.1';
-const OSC_PORT = process.env.OSC_PORT || 41234;
+const OSC_PORT = process.env.OSC_PORT || 8000;
 
 const wss = new WebSocket.Server({ port: WS_PORT });
+
+console.log('Server started. Listening for connections: %s:%d', 'ws:\\\\localhost', WS_PORT);
 
 wss.on('connection', function connection(ws) {
     console.log('Client connected');
 
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
+        console.log('Message received: %s', message);
 
         const buffer = Buffer.from(message);
         const arrayBuffer = new Uint8Array(buffer).buffer;
@@ -27,10 +29,13 @@ wss.on('connection', function connection(ws) {
             const stringData = oscMessage.args[0];
 
             console.log('String received via WebSocket:', stringData);
-            console.log(stringData, 'Sending to:', OSC_HOST, ':', OSC_PORT)
+
             const oscClient = new OSC({ plugin: new OSC.DatagramPlugin({ send: { port: Number(OSC_PORT), host: OSC_HOST } }) });
 
-            oscClient.send(new OSC.Message('/sendText', stringData));
+            oscClient.send(new OSC.Message(oscMessage.address, stringData));
+
+            console.log('Message "%s" sent to OSC: %s:%d on address: %s', stringData, OSC_HOST, OSC_PORT, oscMessage.address);
+
 
 
         } catch (e) {
